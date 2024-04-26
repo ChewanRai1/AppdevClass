79de2c5cdc68fd846c6dd5e5afb311370ec19619
ChewanRai1 <220154@softwarica.edu.np>
2024-04-26 14:03:01 +0545

    container all final

--- lib/Screen/container_screen.dart
+++ lib/Screen/container_screen.dart
@@ -1,3 +1,34 @@
+// import 'package:flutter/material.dart';
+
+// class ContainerScreen extends StatelessWidget {
+//   const ContainerScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.amber[50],
+//       body: Center(
+//         child: SafeArea(
+//           child: Container(
+//             width: 200,
+//             height: 200,
+//             alignment: Alignment.center, //text in the center of container
+
+//             child: const Text('I am super man'),
+//             decoration: BoxDecoration(
+//                 shape: BoxShape.circle,
+//                 color: Colors.amberAccent,
+//                 border: Border.all(
+//                   color: Colors.black,
+//                   width: 2,
+//                 )),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 class ContainerScreen extends StatelessWidget {
@@ -9,17 +40,18 @@ class ContainerScreen extends StatelessWidget {
       backgroundColor: Colors.amber[50],
       body: SafeArea(
         child: Container(
-          width: 200,
-          height: 200,
-          alignment: Alignment.center, //text in the center of container
+          width: double.infinity,
+          height: double.infinity,
+          alignment: Alignment.centerRight, //text in the center of container
 
           child: const Text('I am super man'),
-          decoration: BoxDecoration(
-              color: Colors.amberAccent,
-              border: Border.all(
-                color: Colors.black,
-                width: 2,
-              )),
+          // decoration: BoxDecoration(
+          //     shape: BoxShape.circle,
+          //     color: Colors.amberAccent,
+          //     border: Border.all(
+          //       color: Colors.black,
+          //       width: 2,
+          //     )),
         ),
       ),
     );
