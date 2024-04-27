dad8c50e82b34ce352a845938be3036676bb8c16
ChewanRai1 <220154@softwarica.edu.np>
2024-04-27 08:58:09 +0545

    task for rowand column

---
+++ lib/Screen/task_rowcolumn.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TaskRowcolumn extends StatelessWidget {
+  const TaskRowcolumn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Rows and Column"),
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+}
