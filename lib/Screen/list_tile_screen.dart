fca1838db11b7065c288f1135ab147733dd088ad
ChewanRai1 <220154@softwarica.edu.np>
2024-04-28 12:49:17 +0545

    list tile common and list tile screen connected

--- lib/Screen/list_tile_screen.dart
+++ lib/Screen/list_tile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/app/app.dart';
+import 'package:flutter_application_1/common/listtile_common.dart';
 import 'package:flutter_application_1/common/snackbar.dart';
 
 class ListTileScreen extends StatelessWidget {
@@ -11,25 +12,38 @@ class ListTileScreen extends StatelessWidget {
         appBar: AppBar(
           title: const Text('ListTile'),
         ),
-        body: Column(children: [
-          ListTile(
-            leading: const Icon(Icons.accessible_forward),
-            title: const Text('Chewan Rai'),
-            subtitle: const Text('Kiran Rana'),
-            trailing: const Icon(Icons.delete),
-            onTap: () {
-              debugPrint('ListTile tapped');
-            },
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              for (int i = 0; i < 100; i++)
+                CommonListTile(
+                  index: i,
+                )
+            ],
           ),
-          ListTile(
-            leading: const Icon(Icons.accessible_forward),
-            title: const Text('Kiran Rana'),
-            subtitle: const Text('Chewan Rai'),
-            trailing: const Icon(Icons.delete),
-            onTap: () {
-              debugPrint('2nd ListTile tapped');
-            },
-          ),
-        ]));
+        ));
   }
 }
+
+
+
+        // body: Column(children: [
+        //   ListTile(
+        //     leading: const Icon(Icons.accessible_forward),
+        //     title: const Text('Chewan Rai'),
+        //     subtitle: const Text('Kiran Rana'),
+        //     trailing: const Icon(Icons.delete),
+        //     onTap: () {
+        //       debugPrint('ListTile tapped');
+        //     },
+        //   ),
+        //   ListTile(
+        //     leading: const Icon(Icons.accessible_forward),
+        //     title: const Text('Kiran Rana'),
+        //     subtitle: const Text('Chewan Rai'),
+        //     trailing: const Icon(Icons.delete),
+        //     onTap: () {
+        //       debugPrint('2nd ListTile tapped');
+        //     },
+        //   ),
+        // ]
